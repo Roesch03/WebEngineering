46d57d244ddf994d04158a9ed672bd0c5a529e6d
Marius <marius.roesch01@gmail.com>
2022-11-21 13:44:15 +0100

    import Autowired at LectureController and LectureDateController

--- src/main/java/de/digitra/uniplaner/controller/LectureController.java
+++ src/main/java/de/digitra/uniplaner/controller/LectureController.java
@@ -10,6 +10,7 @@ import de.digitra.uniplaner.service.LecturerService;
 import net.bytebuddy.dynamic.DynamicType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
--- src/main/java/de/digitra/uniplaner/controller/LectureDateController.java
+++ src/main/java/de/digitra/uniplaner/controller/LectureDateController.java
@@ -7,6 +7,7 @@ import de.digitra.uniplaner.interfaces.ILectureDateController;
 import de.digitra.uniplaner.service.LectureDateService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
