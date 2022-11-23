fcbbf65b4958e9ee4644f4edef9e91013cff9610
AnnaKohlbecker <aipskohlbecker@gmail.com>
2022-11-23 13:22:57 +0100

    deleted everything

--- uniplaner_1/myscripts.js
+++ uniplaner_1/myscripts.js
@@ -1,28 +1,3 @@
-/*
-    let lectures =[];
-Test
-const addLecture = (ev)=>{
-    ev.preventDefault();
-    let lecture ={
-        name: document.getElementById('name').value,
-        kurs: document.getElementById('kurs').value,
-        dozent: document.getElementById('dozent').value
-    }
-    lectures.push(lecture);
-    //document.forms[0].reset();
-
-
-    console.warn('added',{lectures});
-
-    let pre = document.querySelector('#msg pre');
-
-    localStorage.setItem('LectureList', JSON.stringify(lectures));
-}
-
-document.addEventListener('DOMContentLoaded', ()=> {
-    document.getElementById('btn').addEventListener('click', addLecture);
-});
-*/
 var selectedRow = null;
 
 function onFormSubmit(){
